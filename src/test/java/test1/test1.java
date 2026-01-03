3ca8ac5d006cb4fa6839b902208663fe448ec2a6
oskay <mustafaoskay6534@gmail.com>
2026-01-03 03:45:20 +0300

    first2 commit

--- src/test/java/test1/test1.java
+++ src/test/java/test1/test1.java
@@ -1,14 +1,16 @@
 package test1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test1 {
-    public static void main(String[] args) {
+  @Test
+  public void gogle(){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         driver.quit();
-    }
+  }
 }
